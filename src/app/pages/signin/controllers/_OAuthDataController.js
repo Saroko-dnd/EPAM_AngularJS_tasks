6d764c77c4231fa4d895565db51e9bed22eb006a
Ihar Saroka <Ihar_Saroka@epam.com>
2018-04-20 17:58:18 +0300

    OAuth: onEnter event handler functionality moved to controller

--- src/app/pages/signin/controllers/_OAuthDataController.js
+++ src/app/pages/signin/controllers/_OAuthDataController.js
@@ -1,6 +1,32 @@
 /* @ngInject */
-export default class OAuthDataController {
-    constructor(clientId) {
-        this.client_id = clientId;
+const OAuthDataController = (
+    $scope,
+    $stateParams,
+    $http,
+    $log,
+    clientId,
+    clientSecret,
+) => {
+    $scope.client_id = clientId;
+
+    if ($stateParams.code) {
+        $http
+            .post('https://github.com/login/oauth/access_token', {
+                code: $stateParams.code,
+                client_id: clientId,
+                client_secret: clientSecret,
+            })
+            .then(
+                (response) => {
+                    $log.log('OAuth Success');
+                    $log.log(response);
+                },
+                (response) => {
+                    $log.log('OAuth Failure');
+                    $log.log(response);
+                },
+            );
     }
-}
+};
+
+export default OAuthDataController;
--- src/app/pages/signin/module.js
+++ src/app/pages/signin/module.js
@@ -6,33 +6,13 @@ import './_variables';
 
 angular
     .module('page.signin', ['ui.router', 'page.signin.variables'])
+    .controller('')
     .config(($stateProvider, $locationProvider) => {
         $stateProvider.state({
             name: 'signin',
             url: '/signin?code',
             templateUrl: './view.html',
             controller: controllers.OAuthDataController,
-            controllerAs: 'OAuthDataController',
-            onEnter: ($stateParams, $http, $log, clientId, clientSecret) => {
-                if ($stateParams.code) {
-                    $http
-                        .post('https://github.com/login/oauth/access_token', {
-                            code: $stateParams.code,
-                            client_id: clientId,
-                            client_secret: clientSecret,
-                        })
-                        .then(
-                            (response) => {
-                                $log.log('OAuth Success');
-                                $log.log(response);
-                            },
-                            (response) => {
-                                $log.log('OAuth Failure');
-                                $log.log(response);
-                            },
-                        );
-                }
-            },
         });
 
         $locationProvider.html5Mode(true);
