73f507cde740d2a173551e4b36eda9a6c75384a5
Ihar Saroka <Ihar_Saroka@epam.com>
2018-05-18 12:42:39 +0300

    OAuth: CORS support was removed from devServer config

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,6 @@ module.exports = {
         historyApiFallback: {
             index: 'index.html',
         },
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers':
-                'Content-Type, Authorization, x-id, Content-Length, X-Requested-With',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        },
     },
     optimization: {
         minimizer: [
